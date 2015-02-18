a93ed7fb24536535dcb13cb4cf0418300ff93f9f
Andy <andygnewman@gmail.com>
2015-02-18 10:43:50 +0000

    add sign in up and out links

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -26,6 +26,7 @@ feature 'reviewing' do
   end
 
   describe Review, :type => :model do
+
     it "is invalid if the rating is more than 5" do
       review = Review.new(rating: 10)
       expect(review).to have(1).error_on(:rating)
