05856fc6b9347d192192739c7857acaad5e27f1e
India Dearlove <india_dearlove@yahoo.co.uk>
2015-02-19 15:19:22 +0000

    adding white stars

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,7 +2,8 @@ module ReviewsHelper
 
   def star_rating(rating)
     return rating unless rating.is_a?(Fixnum)
-    '★' * rating 
+    remainder = (5- rating)
+    '★' * rating + "☆" * remainder 
   end
 
 end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -12,6 +12,14 @@ describe ReviewsHelper, type: :helper do
       expect(helper.star_rating(5)).to eq '★★★★★'
     end
 
+    it 'returns three black stars and two white stars for three' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+
+    it 'returns four black stars and one white star for 3.5' do
+      expect(helper.star_rating(3.5)).to eq "★★★★☆"
+    end
+
   end
 
 end
